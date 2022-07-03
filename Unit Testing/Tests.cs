3142075b01da00e5d9b26a4e178f0f2fdc81384e
hendrianiyan <hendrian582@gmail.com>
2022-07-03 21:27:35 +0700

    membuat test pada addUser

--- Unit Testing/Tests.cs
+++ Unit Testing/Tests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using APL;
 using NUnit.Framework;
 
 namespace Unit_Testing
@@ -12,18 +12,30 @@ namespace Unit_Testing
             int a = 200;
             int b = 100;
             int c = Count.Math.pertambahan(a, b);
-            
+
             Assert.AreEqual(300, c);
         }
-        
+
         [Test]
         public void Pengurangan()
-        {   
+        {
             int a = 4000;
             int b = 3000;
             int c = Count.Math.pengurangan(a, b);
-            
+
             Assert.AreEqual(1000, c);
+        }
+
+        [Test]
+        public void addUser()
+        {
+            string a = "A";
+            string b = "B";
+            string c = "C";
+            string d = "D";
+            string e = "E";
+
+            pengguna.addUser(a, b, c, d, e);
         }
     }
 }
