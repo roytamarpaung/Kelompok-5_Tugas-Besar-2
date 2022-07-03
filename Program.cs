52689cc33bb43be12864082492cc0c97b12dc6b5
roytamarpaung <100549964+roytamarpaung@users.noreply.github.com>
2022-07-03 21:02:34 +0700

    Commit Pertama

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace APL
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            //run the application
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //login form
+            Application.Run(new LoginForm()); 
+        }
+    }
+}
