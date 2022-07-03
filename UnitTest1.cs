55f439a5c9253af72980d2493c063bc69b396e7d
roytamarpaung <100549964+roytamarpaung@users.noreply.github.com>
2022-07-03 21:06:24 +0700

    Menambahkan Unit Test

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TransaksiTest
+{
+    [TestClass]
+    public class TambahSaldo
+    {
+        public static int Jumlah(int a, int b) 
+        { 
+        
+        }
+        [TestMethod]
+        public void TestTambahSaldo()
+        {
+             
+        }
+    }
+}
